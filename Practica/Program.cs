98735634041697bcd23a45a5f76c03aa5d601a61
Boris Hernández <borisnandez93@gmail.com>
2021-10-05 08:38:34 -0600

    Add project files.

---
+++ Practica/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+
+public class Example
+
+{
+
+    public static void Main()
+
+    {
+
+        ArrayList estudiantes = new ArrayList();
+
+        estudiantes.Add("Juan");
+
+        estudiantes.Add("Maria");
+
+        estudiantes.Add("Pdro");
+
+        estudiantes.Add("Luis");
+
+        estudiantes[2] = "Pedro";
+
+        for (int i = 0; i < estudiantes.Count; i ++)
+
+        {
+
+            Console.WriteLine("Estudiante {0} es \"{1}\"", i,
+
+                estudiantes[i]);
+
+        }
+
+    }
+
+}
