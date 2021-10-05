9b7cbeae0d623c179556b4132b2320a79a2d7330
Boris Hernández <borisnandez93@gmail.com>
2021-10-05 09:30:03 -0600

    cambios

--- Practica-2/Practica-2/Program.cs
+++ Practica-2/Practica-2/Program.cs
@@ -6,7 +6,26 @@ namespace Practica_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int nElmentos;
+            Console.Write("Digite numero de elementos de la matriz:");
+            nElmentos = Convert.ToInt32(Console.ReadLine());
+            int[] m = new int[nElmentos]; // crear la matriz 
+            int i = 0; //subindices 
+
+            // llenar la matriz 
+            Console.WriteLine("Introducir los valores de la matriz");
+            for (i = 0; i < nElmentos; i++)
+            {
+                Console.Write("m[" + i + "]=");
+                m[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            //ver elementos de una matriz 
+            Console.WriteLine();
+            for (i = 0; i < nElmentos; i++)
+            {
+                Console.Write(m[i] + "");
+            }
         }
     }
 }
